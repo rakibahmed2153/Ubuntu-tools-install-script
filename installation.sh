0275d4d0ec6e9889b41c85567f1fdfbf259aa1d6
Rakib Ahmed <36979611+rakibahmed2153@users.noreply.github.com>
2023-08-01 13:00:25 +0600

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,129 @@
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt dist-upgrade -y
+
+sudo apt install ltrace -y
+
+sudo wget -q -O - https://screenrec.com/download/pub.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://screenrec.com/download/ubuntu stable main'
+sudo apt install screenrec -y
+
+sudo apt install ruby
+sudo apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev  libgmp-dev zlib1g-dev
+sudo gem install wpscan
+
+sudo apt-get install docker.io docker-compose -y
+ 
+sudo pip3 install mitmproxy2swagger
+
+sudo apt-get install git -y
+
+sudo apt-get install docker.io docker-compose -y
+
+sudo apt install golang-go -y
+ 
+sudo apt install zaproxy -y
+
+sudo apt-get install android-tools-adb -y
+
+sudo apt install golang git build-essential libpcap-dev libusb-1.0-0-dev libnetfilter-queue-dev -y
+
+sudo snap install metasploit-framework -y
+
+sudo apt install tor torbrowser-launcher -y
+
+sudo apt install proxychains4 -y
+
+sudo apt install sqlmap -y
+
+sudo apt install hash-identifier -y
+
+sudo apt install curl
+
+sudo apt install figlet
+
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+
+sudo apt-get install nodejs -Y
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/lsb_release -cs-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+
+sudo apt-get install postgresql postgresql-contrib -Y
+
+sudo apt install net-tools -Y
+
+sudo apt install ettercap-graphical -Y
+
+sudo apt install nmap -Y
+
+sudo apt install python3-pip -y
+
+sudo apt install npm -y
+
+sudo apt install libimage-exiftool-perl -y
+
+sudo apt install steghide -y
+
+sudo apt install binwalk -y
+
+sudo gem install zsteg
+
+sudo apt install gmic -y
+
+sudo apt install wireshark -y
+
+sudo apt install jq -y
+
+sudo apt  install awscli -y
+
+sudo apt  install apktool -y
+
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+sudo apt -y install gcc g++ flex libewf-dev -y
+
+sudo apt install tesseract-ocr libtesseract-dev tesseract-ocr-eng -y
+
+sudo apt install gnome-tweaks -y
+
+sudo apt-get install chrome-gnome-shell -y
+
+sudo apt-get install -y dnsmap
+
+sudo apt-get install -y aircrack-ng
+
+sudo apt-get install -y hydra
+
+sudo apt-get install -y john
+
+sudo apt-get install -y ettercap
+
+sudo apt-get install -y openvas-cli
+
+sudo apt-get install -y netcat
+
+sudo apt-get install -y nikto
+
+sudo apt-get install -y netsniff-ng
+
+sudo apt-get install -y foremost
+
+sudo apt-get install -y binwalk
+
+sudo apt-get install mimikatz -y
+
+sudo apt-get install recon-ng -y
+
+sudo apt-get install metasploit-framework -y
+
+sudo apt-get install hashcat -y
+
+sudo apt-get install exploitdb -y
+
+sudo apt-get install crunch -y
+
+sudo apt-get install apktool -y
+
+sudo apt-get install whois -y
